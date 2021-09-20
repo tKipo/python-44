93a884f35c8da89593db884036a0fe7685ed463b
Kiryl Yankouski <truekipo@gmail.com>
2021-09-20 20:46:08 +0300

    Yo!

--- Crashed_Clock.py
+++ Crashed_Clock.py
@@ -3,5 +3,9 @@ Hours = Number // 3600
 Minutes = (Number - Hours * 3600) // 60
 Seconds = Number - Hours * 3600 - Minutes * 60
 print(Hours % 24, ":", Minutes, ":", Seconds)
+<<<<<<< HEAD
 print("Good news everyone?!")
 #check out
+=======
+print("Good news everyone")
+>>>>>>> parent of aab2804 (Update Crashed_Clock.py)
