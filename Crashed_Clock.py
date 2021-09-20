aab2804440749f4f9c0c3077af14bd39a83c61c3
tKipo <91083169+tKipo@users.noreply.github.com>
2021-09-20 19:57:33 +0300

    Update Crashed_Clock.py

--- Crashed_Clock.py
+++ Crashed_Clock.py
@@ -3,4 +3,4 @@ Hours = Number // 3600
 Minutes = (Number - Hours * 3600) // 60
 Seconds = Number - Hours * 3600 - Minutes * 60
 print(Hours % 24, ":", Minutes, ":", Seconds)
-print("Good news everyone")
+print("Good news everyone!")
